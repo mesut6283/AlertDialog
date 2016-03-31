b15eeaad46b7b964a3626f0886ce8069c5e64fd5
mesut6283 <masoud_m_tr@yahoo.com>
2016-04-01 00:53:57 +0300

    porsesh hengame khoruj az barname

--- app/src/main/java/com/masoud/example/alertdialog/MainActivity.java
+++ app/src/main/java/com/masoud/example/alertdialog/MainActivity.java
@@ -1,48 +1,57 @@
 package com.masoud.example.alertdialog;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public MediaPlayer player;
-    public int playok=0;
+    public int playok = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button b=(Button)findViewById(R.id.button1);
+        Button b = (Button) findViewById(R.id.button1);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(playok==0){
-                player=MediaPlayer.create(MainActivity.this,R.raw.pat_mat);
-                player.setLooping(true);
-                player.setVolume(100,100);
-                player.start();
-                playok=1;}
+                if (playok == 0) {
+                    player = MediaPlayer.create(MainActivity.this, R.raw.pat_mat);
+                    player.setLooping(true);
+                    player.setVolume(100, 100);
+                    playok = 1;
+                    player.start();
+
+                }
 
                 ShowAlertDialog();
             }
         });
 
-Button stopButton=(Button)findViewById(R.id.button2);
-        stopButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                player.stop();
-                playok=0;
-            }
-        });
+        Button stopButton = (Button) findViewById(R.id.button2);
+
+            stopButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (playok == 1) {
+                        player.stop();
+                        playok = 0;
+                    }
+                }
+            });
+
     }
-    public void ShowAlertDialog(){
-        AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+
+    public void ShowAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
 
         builder.setTitle(R.string.title);
         builder.setMessage(R.string.message);
@@ -52,22 +61,44 @@ Button stopButton=(Button)findViewById(R.id.button2);
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
-                Toast.makeText(getApplicationContext(),"بله شما خنگول هستین خودش هم از نوع بز",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "بله شما خنگول هستین خودش هم از نوع بز", Toast.LENGTH_SHORT).show();
             }
         });
         builder.setNegativeButton(R.string.No, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
-                Toast.makeText(getApplicationContext(),"فکر کردی خیال کردی معلومه که خنگولی",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "فکر کردی خیال کردی معلومه که خنگولی", Toast.LENGTH_LONG).show();
 
             }
         });
 
 
+        builder.show();
 
-builder.show();
+    }
 
+
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this).setIcon(R.mipmap.ic_launcher).setTitle("Exit")
+                .setMessage("خنگول مطمئنی ؟")
+                .setPositiveButton("بله", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        Intent intent = new Intent(Intent.ACTION_MAIN);
+                        intent.addCategory(Intent.CATEGORY_HOME);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+                }).setNegativeButton("خیر", null).show();
+        if(playok==1) {
+            player.stop();
+            playok = 0;
+        }
     }
 
 }
